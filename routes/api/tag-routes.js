7b24aa641f7aee1907b8d75aabbcd31a797e5d7d
Jordan DuBose <jordan.dubose1@gmail.com>
2021-10-31 16:04:39 -0400

    completing tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   // find all tags
   Tag.findAll({
     // be sure to include its associated Product data
-    include: { model: Tag, through: ProductTag, as: 'tagged_product' }
+    include: { model: Product, through: ProductTag, as: 'tagged_product' }
   })
   .then((tagData) => {
     res.json(tagData);
@@ -20,19 +20,62 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    // be sure to include its associated Product data
+    include: { model: Product, through: ProductTag, as: 'tagged_product' }
+  })
+  .then((tagData) => {
+    res.json(tagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then((tagData) => {
+      // Send the newly created row as a JSON object
+      res.json(tagData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagData) => {
+      // Send the newly created row as a JSON object
+      res.json(tagData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagData) => {
+    // Send the newly created row as a JSON object
+    res.json(tagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
